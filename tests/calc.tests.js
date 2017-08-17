84e8c56c3c12337a6f84cfc91bc0b209e867f797
Lizaveta Parshankova <divine.cow@yandex.ru>
2017-08-17 22:22:04 +0300

    Test for function ifNumber

--- tests/calc.tests.js
+++ tests/calc.tests.js
@@ -5,3 +5,12 @@ describe('last', function () {
     expect(result).to.be(2);
   });
 });
+
+describe('ifNumber', function () {
+  it('makes value a new element of the buffer', function () {
+    expect(ifNumber([], '5')).to.eql(['5']);
+    expect(ifNumber(['1', '+'], '5')).to.eql(['1', '+', '5']);
+  });
+
+  
+})
