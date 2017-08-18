8e19ecb60e46cb921755a1d6d322bb14f16161c0
Lizaveta Parshankova <divine.cow@yandex.ru>
2017-08-18 12:24:24 +0300

    Grammar nazi alert

--- tests/calc.tests.js
+++ tests/calc.tests.js
@@ -12,12 +12,12 @@ describe('ifNumber', function () {
     expect(ifNumber(['1', '+'], '5')).to.eql(['1', '+', '5']);
   });
 
-  it('concatenate value with last element of the buffer', function () {
+  it('concatenates value with last element of the buffer', function () {
     expect(ifNumber(['25'], '5')).to.eql(['255']);
     expect(ifNumber(['1', '+', '1'], '5')).to.eql(['1', '+', '15']);
   });
 
-  it('replase zero element with value', function () {
+  it('replaces zero element with value', function () {
     expect(ifNumber(['0'], '5')).to.eql(['5']);
     expect(ifNumber(['1', '+', '0'], '5')).to.eql(['1', '+', '5']);
   });
