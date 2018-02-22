85f4e38f32a37b46cf831b4f70e821d082fa4ea1
Elizaveta Parshenkova <Elizaveta_Parshenkova@epam.com>
2018-02-22 12:56:48 +0300

    Send error if parameter 'action' exists but it's not correct.

--- calculate.js
+++ calculate.js
@@ -8,15 +8,27 @@ app.get('/calculate', (req, res) => {
 
     let result = 0;
 
-    switch (action) {
-        case 'add': result = left + right;
-        break;
-        case 'substract': result = left - right;
-        break;
-        case 'multiply': result = left * right;
-        break;
-        case 'divide': result = left / right;
-        break;
+    if (action) {
+        switch (action) {
+            case 'add': result = left + right;
+                break;
+            case 'substract': result = left - right;
+                break;
+            case 'multiply': result = left * right;
+                break;
+            case 'divide': result = left / right;
+                break;
+            default: res.status(422).json({
+                "errors": [
+                    {
+                        "status": "422",
+                        "title": "Invalid parameter value",
+                        "detail": "\'" + action + "\'" + " is an unknown action"
+                    }
+                ]
+            });
+                return
+        }
     }
 
     res.status(200).json({'data': result});
